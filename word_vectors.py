d260e98065dfffb7f6651e0e61240a7d3031cc3a
koziscool <kozjob@yahoo.com>
2016-08-31 15:19:03 -0500

    more

--- word_vectors.py
+++ word_vectors.py
@@ -1,4 +1,11 @@
+import numpy as np 
 import pandas as pd
+# Import various modules for string cleaning
+from bs4 import BeautifulSoup
+import re
+from nltk.corpus import stopwords
+import nltk.data
+
 
 # Read data from files 
 train = pd.read_csv( "labeledTrainData.tsv", header=0, 
@@ -11,3 +18,66 @@ unlabeled_train = pd.read_csv( "unlabeledTrainData.tsv", header=0,
 print "Read %d labeled train reviews, %d labeled test reviews, " \
     "and %d unlabeled reviews\n" % (train["review"].size,  
     test["review"].size, unlabeled_train["review"].size )
+
+
+def review_to_wordlist( review, remove_stopwords=False ):
+    # Function to convert a document to a sequence of words,
+    # optionally removing stop words.  Returns a list of words.
+    #
+    # 1. Remove HTML
+    review_text = BeautifulSoup(review).get_text()
+    #  
+    # 2. Remove non-letters
+    review_text = re.sub("[^a-zA-Z]"," ", review_text)
+    #
+    # 3. Convert words to lower case and split them
+    words = review_text.lower().split()
+    #
+    # 4. Optionally remove stop words (false by default)
+    if remove_stopwords:
+        stops = set(stopwords.words("english"))
+        words = [w for w in words if not w in stops]
+    #
+    # 5. Return a list of words
+    return(words)
+
+# nltk.download() 
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+
+# Define a function to split a review into parsed sentences
+def review_to_sentences( review, tokenizer, remove_stopwords=False ):
+    # Function to split a review into parsed sentences. Returns a 
+    # list of sentences, where each sentence is a list of words
+    #
+    # 1. Use the NLTK tokenizer to split the paragraph into sentences
+    raw_sentences = tokenizer.tokenize(review.strip())
+    #
+    # 2. Loop over each sentence
+    sentences = []
+    for raw_sentence in raw_sentences:
+        # If a sentence is empty, skip it
+        if len(raw_sentence) > 0:
+            # Otherwise, call review_to_wordlist to get a list of words
+            sentences.append( review_to_wordlist( raw_sentence, \
+              remove_stopwords ))
+    #
+    # Return the list of sentences (each sentence is a list of words,
+    # so this returns a list of lists
+    return sentences
+
+
+sentences = []  # Initialize an empty list of sentences
+
+print "Parsing sentences from training set"
+for review in train["review"]:
+    sentences += review_to_sentences(review.decode("utf-8"), tokenizer)
+
+print "Parsing sentences from unlabeled set"
+for review in unlabeled_train["review"]:
+    sentences += review_to_sentences(review, tokenizer)
+
+
+print len( sentences )
+print sentences[0]
+print sentences[1]
